e16338050ab23e9d784e346df75a2a4bef9702ce
Koushik Ruj <70309662+Ethicalhackerkoushik@users.noreply.github.com>
2020-12-17 15:42:49 +0530

    Update and rename .IP-Tracer.php to .IP-Hunter.php

---
+++ .IP-Hunter.php
@@ -0,0 +1,97 @@
+<?php
+include("modules/trm.php");
+include("modules/help.php");
+include("modules/trip.php");
+
+function logo() {
+  system("clear");
+  echo 
+
+
+
+echo "██╗██████╗       ██╗  ██╗██╗   ██╗███╗   ██╗████████╗███████╗██████╗ ";
+echo "██║██╔══██╗      ██║  ██║██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗";
+echo "██║██████╔╝█████╗███████║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝";
+echo "██║██╔═══╝ ╚════╝██╔══██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗";
+echo "██║██║           ██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║";
+echo "╚═╝╚═╝           ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝";
+echo "
+echo "                                                     @Made By Koushik";
+echo "                                                      version:-20.01.2
+echo "    Follow On GitHub:- https://github.com/Ethicalhackerkoushik";
+
+      
+
+
+echo <<<EOL
+
+
+   \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
+}\033[01;31m-------------- \033[01;32mTrack IPLocation\033[01;31m --------------\033[01;37m{
+   }\033[01;31m----------------------------------------\033[01;37m{
+
+\033[00m
+EOL;
+}
+
+function About() {
+  logo();
+  echo <<<EOL
+
+         \033[01;33mTool Name \033[01;37m:- \033[01;36mIP-Hunter
+         \033[01;33mAuthor \033[01;37m:- \033[01;36mKoushik Ruj
+         \033[01;33mPowered By \033[01;37m:- \033[01;36mTechnicalHub
+
+ \033[01;33mIP-Hunter\033[01;32m is use \033[01;36mip-api \033[01;32mto retrive any ip address information. Our system will automatically ban any IP addresses doing over 150 requests per minute. Modify By ©Koushik \033[00m
+
+
+EOL;
+  $getact = readline('  IP-Hunter >> ');
+  menu();
+}
+
+function upd() {
+  logo();
+  echo "\n\033[01;32mUpdating IP-Hunter.........\033[01;37m\n\n";
+  sleep(1);
+  system("cd ~/ && git clone https://github.com/ethicalhackerkoushik/IP-Hunter.git");
+  system("cd ~/ && sudo git clone https://github.com/ethicalhackerkoushik/IP-Hunter.git");
+  system("cd ~/IP-Hunter && sh install");
+  logo();
+  echo "\n\033[01;32m              IP-Hunter updated !!!\033[01;37m\n";
+  sleep(1);
+  menu();
+}
+
+function menu() {
+  logo();
+  echo "   \033[01;32m[ \033[01;37m1 \033[01;32m] \033[01;33mTrack IP Address.\n";
+  echo "   \033[01;32m[ \033[01;37m2 \033[01;32m] \033[01;33mTrack Your IP Address.\n";
+  echo "   \033[01;32m[ \033[01;37m3 \033[01;32m] \033[01;33mAbout us.\n";
+  echo "   \033[01;32m[ \033[01;37m4 \033[01;32m] \033[01;33mHelp.\n";
+  echo "   \033[01;32m[ \033[01;37m5 \033[01;32m] \033[01;33mUpdate IP-Hunter.\n";
+  echo "   \033[01;32m[ \033[01;37mx \033[01;32m] \033[01;33mExit \n\n\033[00m";
+  $inp = readline('  IP-Hunter >> ');
+  if ($inp=="x" || $inp=="exit") {
+    echo "\n\033[01;31m  Exiting .......\033[00m\n";
+    sleep(1);
+    echo "\033[01;32m  By ....... :)\n\n\033[00m";
+    exit();
+  } else if ($inp=="1") {
+    trac();
+  } else if ($inp=="2") {
+    tracm();
+  } else if ($inp=="3") {
+    About();
+  } else if ($inp=="4") {
+    help();
+  } else if ($inp=="5") {
+    upd();
+  } else {
+    echo "\n  \033[01;31mErr : Invalid Command \033[01;32m'$inp'\033[00m";
+    sleep(1);
+    menu();
+  }
+}
+menu();
+?>
